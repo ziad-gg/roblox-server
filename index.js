43de5b40a1186f214c5263bb01debcc31de97d16
ziad <99255613+ziad-gg@users.noreply.github.com>
2024-07-18 17:13:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 app.get('/v1/groups/:id', async (req, res) => {
     const id = req.params.id;
 
-    const response = await axios.get(`https://groups.roproxy.com/v1/groups/${id}`).catch(e => e.response);
+    const response = await axios.get(`https://groups.roblox.com/v1/groups/${id}`).catch(e => e.response);
 
     res.status(response.status).json({ data: response.data, status: response.status });
 });
