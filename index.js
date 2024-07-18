25dbab1e43a74374f273825bc12922faf01e1498
ziad <99255613+ziad-gg@users.noreply.github.com>
2024-07-18 16:47:31 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { default: axios } = require('axios');
+
+const app = express();
+
+app.use(express.json({ limit: '10MB' }));
+
+const port = process.env.PORT || 3000;
+
+app.get('/v1/groups/:id', async (req, res) => {
+    const id = req.params.id;
+
+    const response = await axios.get(`https://groups.roblox.com/v1/groups/${id}`).catch(e => e.response);
+
+    res.status(response.status).json({ data: response.data, status: response.status });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
